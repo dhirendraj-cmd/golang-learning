a58db0cdeac25ddd365649b4123a73fd1ed27945
dhirendraj-cmd <djha5051@gmail.com>
2025-09-02 17:19:42 +0530

    added types and basic operations

--- 01-helloworld/main.go
+++ 01-helloworld/main.go
@@ -3,6 +3,25 @@ package main
 import "fmt"
 
 func main() {
+
+	// string
 	fmt.Print("Hello !!!!\n")
 	fmt.Println("This will print in new line without \\n ")
+
+	// string concat
+	fmt.Print("This " + "is " + "go" + " Tut")
+
+	// int, float
+	fmt.Println(10)
+	fmt.Println(10.99)
+	fmt.Println(10 + 12)
+	fmt.Println(10 + 12.33)
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println(false && true)
+	fmt.Println(true && false)
+	fmt.Println(false || true)
+	fmt.Println(true || false)
 }
